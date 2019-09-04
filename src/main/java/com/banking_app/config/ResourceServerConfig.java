e7e309f1dd799bdd9707f264d61e7f786dbf71a8
Tonykaynoni <tonykay001@gmail.com>
2019-09-04 18:27:13 +0100

    boss

--- src/main/java/com/banking_app/config/ResourceServerConfig.java
+++ src/main/java/com/banking_app/config/ResourceServerConfig.java
@@ -1,11 +1,16 @@
 package com.banking_app.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -22,9 +27,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
         http.             
                 authorizeRequests()
-                .antMatchers("/login","/register","/reg_user","/","/static/**","/assets/**","/img/**","/files/**").permitAll()
+                .antMatchers("/login","/register","/reg_user","/","/static/**","/assets/**","/img/**","/files/**","/myApiText").permitAll()
                 .anyRequest().authenticated().and().httpBasic()         
                 .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler()); 
-	}
-
+	} 
+	
+  
 }
--- src/main/java/com/banking_app/controller/MainController.java
+++ src/main/java/com/banking_app/controller/MainController.java
@@ -1,10 +1,19 @@
 package com.banking_app.controller;
 
+import java.io.IOException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 	
+	 @PostMapping("/myApiText")
+	    public String listUploadedFiles(Model model) throws IOException {
 
+	       
+
+	        return "success";
+	    }
 
 }
